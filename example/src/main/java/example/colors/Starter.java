6c494db50557a45f0dd725f48c9ef231773de92e
floriankirmaier <florian.kirmaier@gmail.com>
2022-11-19 19:58:41 +0100

    Added a java version of the color sample

---
+++ example/src/main/java/example/colors/Starter.java
@@ -0,0 +1,23 @@
+package example.colors;
+
+import com.jpro.routing.WebApp;
+import com.jpro.routing.sessionmanager.SessionManager;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+class Starter extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        WebApp app = new ColorsApp(stage);
+        Scene scene = new Scene(app, 1400,800);
+        stage.setScene(scene);
+        stage.show();
+        app.start(SessionManager.getDefault(app,stage));
+    }
+  
+}
