cb72c9b1a8db2939954f05b8c03f9164b432d89d
floriankirmaier <florian.kirmaier@gmail.com>
2022-11-24 22:29:20 +0100

    Added a simple hamburger menu sample

--- example/src/main/java/example/colors/Starter.java
+++ example/src/main/java/example/colors/Starter.java
@@ -6,7 +6,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-class Starter extends Application {
+public class Starter extends Application {
     public static void main(String[] args) {
         launch(args);
     }
