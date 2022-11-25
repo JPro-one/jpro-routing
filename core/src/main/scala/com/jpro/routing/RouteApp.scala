7af4204eccca61546a533d24ee757fdb5c1314e8
floriankirmaier <florian.kirmaier@gmail.com>
2022-11-25 18:50:34 +0100

    Fixed getWebAPI, renamed RouteApp.getNode to RouteApp.createRoute

--- core/src/main/scala/com/jpro/routing/RouteApp.scala
+++ core/src/main/scala/com/jpro/routing/RouteApp.scala
@@ -8,20 +8,20 @@ import javafx.stage.Stage
 
 abstract class RouteApp extends Application {
 
-  private var _stage = null;
+  private var _stage: Stage = null;
   def getStage(): Stage = _stage
 
   def getWebAPI(): WebAPI = WebAPI.getWebAPI(getStage())
 
   override def start(stage: Stage): Unit = {
-    _stage = null
+    _stage = stage
     val routeNode = new RouteNode(stage)
-    routeNode.setRoute(getRoute())
+    routeNode.setRoute(createRoute())
     val scene = new Scene(routeNode, 1400, 800)
     stage.setScene(scene)
     stage.show()
     routeNode.start(SessionManager.getDefault(routeNode, stage))
   }
 
-  def getRoute(): Route
+  def createRoute(): Route
 }
