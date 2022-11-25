da9b67027787999fa3e89efb592a7e7b3346352f
floriankirmaier <florian.kirmaier@gmail.com>
2022-11-25 18:31:30 +0100

    Added getStage and getWebAPI to the RouteApp

--- core/src/main/scala/com/jpro/routing/RouteApp.scala
+++ core/src/main/scala/com/jpro/routing/RouteApp.scala
@@ -1,13 +1,20 @@
 package com.jpro.routing
 
 import com.jpro.routing.sessionmanager.SessionManager
+import com.jpro.webapi.WebAPI
 import javafx.application.Application
 import javafx.scene.Scene
 import javafx.stage.Stage
 
 abstract class RouteApp extends Application {
 
+  private var _stage = null;
+  def getStage(): Stage = _stage
+
+  def getWebAPI(): WebAPI = WebAPI.getWebAPI(getStage())
+
   override def start(stage: Stage): Unit = {
+    _stage = null
     val routeNode = new RouteNode(stage)
     routeNode.setRoute(getRoute())
     val scene = new Scene(routeNode, 1400, 800)
