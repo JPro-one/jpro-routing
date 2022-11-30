7cdc50afd6b94d84663b53dcc6a039c3878f98f9
Besmir Beqiri <besmir.beqiri@logicbind.io>
2022-11-30 13:12:18 +0100

    Configuration the Microsoft OAuth2 authentication provider

---
+++ auth/src/main/java/one/jpro/auth/oath2/provider/MicrosoftAuthenticationProvider.java
@@ -0,0 +1,45 @@
+package one.jpro.auth.oath2.provider;
+
+import one.jpro.auth.authentication.AuthenticationProvider;
+import one.jpro.auth.oath2.JWTOptions;
+import one.jpro.auth.oath2.OAuth2AuthenticationProvider;
+import one.jpro.auth.oath2.OAuth2Flow;
+import one.jpro.auth.oath2.OAuth2Options;
+
+/**
+ * Simplified factory to create an {@link AuthenticationProvider} for Microsoft.
+ *
+ * @author Besmir Beqiri
+ */
+public class MicrosoftAuthenticationProvider extends OAuth2AuthenticationProvider {
+
+    /**
+     * Create an {@link OAuth2AuthenticationProvider} for Microsoft.
+     *
+     * @param options custom OAuth2 options
+     */
+    public MicrosoftAuthenticationProvider(OAuth2Options options) {
+        super(options);
+    }
+
+    /**
+     * Create an {@link OAuth2AuthenticationProvider} for Microsoft.
+     *
+     * @param clientId          the client id given to you by Google
+     * @param clientSecret      the client secret given to you by Google
+     * @param tenant            the guid of your application
+     */
+    public MicrosoftAuthenticationProvider(String clientId, String clientSecret, String tenant) {
+        super(new OAuth2Options()
+                .setFlow(OAuth2Flow.AUTH_CODE)
+                .setClientId(clientId)
+                .setClientSecret(clientSecret)
+                .setTenant(tenant)
+                .setSite("https://login.microsoftonline.com/{tenant}")
+                .setTokenPath("/oauth2/token")
+                .setAuthorizationPath("/oauth2/authorize")
+                .setJwkPath("/../common/discovery/keys")
+                .setJwtOptions(new JWTOptions()
+                        .setNonceAlgorithm("SHA-256")));
+    }
+}
