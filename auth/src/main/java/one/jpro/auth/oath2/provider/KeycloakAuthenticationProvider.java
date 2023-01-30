f7d5920e539f80fe8af4fd5813eb155b654470ce
Besmir Beqiri <besmir.beqiri@logicbind.io>
2023-01-30 20:23:54 +0100

    Implement Keycloak OAuth2 authentication provider

---
+++ auth/src/main/java/one/jpro/auth/oath2/provider/KeycloakAuthenticationProvider.java
@@ -0,0 +1,100 @@
+package one.jpro.auth.oath2.provider;
+
+import com.jpro.webapi.WebAPI;
+import one.jpro.auth.authentication.AuthenticationProvider;
+import one.jpro.auth.oath2.OAuth2AuthenticationProvider;
+import one.jpro.auth.oath2.OAuth2Flow;
+import one.jpro.auth.oath2.OAuth2Options;
+import one.jpro.auth.oath2.PubSecKeyOptions;
+import org.json.JSONObject;
+
+/**
+ * Simplified factory to create an {@link AuthenticationProvider} for Keycloak.
+ *
+ * @author Besmir Beqiri
+ */
+public class KeycloakAuthenticationProvider extends OAuth2AuthenticationProvider {
+
+    /**
+     * Create an {@link OAuth2AuthenticationProvider} for Keycloak.
+     *
+     * @param webAPI  the JPro WebAPI
+     * @param options custom OAuth2 options
+     */
+    public KeycloakAuthenticationProvider(final WebAPI webAPI, final OAuth2Options options) {
+        super(webAPI, options);
+    }
+
+    /**
+     * Create an {@link OAuth2AuthenticationProvider} for Keycloak.
+     *
+     * @param webAPI the JPro WebAPI
+     * @param config the json configuration exported from Keycloak admin console
+     */
+    public KeycloakAuthenticationProvider(final WebAPI webAPI, final JSONObject config) {
+        this(webAPI, OAuth2Flow.AUTH_CODE, config);
+    }
+
+    /**
+     * Create an {@link OAuth2AuthenticationProvider} for Keycloak.
+     *
+     * @param webAPI the JPro WebAPI
+     * @param flow   the OAuth2 flow to use
+     * @param config the JSON configuration exported from Keycloak admin console
+     */
+    public KeycloakAuthenticationProvider(final WebAPI webAPI, final OAuth2Flow flow, final JSONObject config) {
+        super(webAPI, configure(flow, config));
+    }
+
+    /**
+     * Create an {@link OAuth2Options} from a JSON configuration exported from Keycloak admin console.
+     *
+     * @param flow   the OAuth2 flow to use
+     * @param config the json configuration exported from Keycloak admin console
+     * @return the OAuth2 options
+     */
+    private static OAuth2Options configure(final OAuth2Flow flow, final JSONObject config) {
+        final OAuth2Options options = new OAuth2Options();
+        options.setFlow(flow);
+
+        if (config.has("resource")) {
+            options.setClientId(config.getString("resource"));
+        }
+
+        // keycloak conversion to OAuth2 options
+        if (config.has("auth-server-url")) {
+            options.setSite(config.getString("auth-server-url"));
+        }
+
+        if (config.has("credentials") && config.getJSONObject("credentials").has("secret")) {
+            options.setClientSecret(config.getJSONObject("credentials").getString("secret"));
+        }
+
+        if (config.has("realm")) {
+            final String realm = config.getString("realm");
+
+            options.setAuthorizationPath("/realms/" + realm + "/protocol/openid-connect/auth");
+            options.setTokenPath("/realms/" + realm + "/protocol/openid-connect/token");
+            // no revocation path
+            options.setRevocationPath(null);
+            options.setUserInfoPath("/realms/" + realm + "/protocol/openid-connect/userinfo");
+            options.setLogoutPath("/realms/" + realm + "/protocol/openid-connect/logout");
+            // keycloak follows the RFC7662 (https://www.rfc-editor.org/rfc/rfc7662)
+            options.setIntrospectionPath("/realms/" + realm + "/protocol/openid-connect/token/introspect");
+            // keycloak follows the RFC7517 (https://www.rfc-editor.org/rfc/rfc7517)
+            options.setJwkPath("/realms/" + realm + "/protocol/openid-connect/certs");
+        }
+
+        if (config.has("realm-public-key")) {
+            options.addPubSecKeys(new PubSecKeyOptions()
+                    .setAlgorithm("RS256")
+                    .setBuffer(
+                            // wrap the key with the right boundaries:
+                            "-----BEGIN PUBLIC KEY-----\n" +
+                            config.getString("realm-public-key") +
+                            "\n-----END PUBLIC KEY-----\n"
+                    ));
+        }
+        return options;
+    }
+}
