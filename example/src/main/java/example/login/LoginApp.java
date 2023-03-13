8855fca18cf6103ed64776f78431ad745dfab6ce
Besmir Beqiri <besmir.beqiri@logicbind.io>
2023-03-13 10:32:00 +0100

    Add an `error routing node` to show any error during the authorization flow

--- example/src/main/java/example/login/LoginApp.java
+++ example/src/main/java/example/login/LoginApp.java
@@ -1,26 +1,34 @@
 package example.login;
 
-import one.jpro.routing.Filters;
-import one.jpro.routing.Route;
-import one.jpro.routing.RouteApp;
 import example.auth.AuthFilters;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import one.jpro.auth.AuthAPI;
+import one.jpro.auth.authentication.User;
 import one.jpro.auth.oath2.OAuth2Credentials;
 import one.jpro.auth.oath2.provider.GoogleAuthenticationProvider;
 import one.jpro.auth.oath2.provider.MicrosoftAuthenticationProvider;
+import one.jpro.routing.Filters;
+import one.jpro.routing.Route;
+import one.jpro.routing.RouteApp;
+import one.jpro.routing.dev.DevFilter;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.URL;
 import java.util.List;
+import java.util.Optional;
 
 import static one.jpro.routing.RouteUtils.getNode;
 
@@ -38,14 +46,15 @@ public class LoginApp extends RouteApp {
     private static final String AZURE_CLIENT_SECRET = System.getenv("AZURE_TEST_CLIENT_SECRET");
 //    private static final String AZURE_TENANT = System.getenv("AZURE_TEST_CLIENT_TENANT");
 
-    private final StringProperty nameProperty = new SimpleStringProperty(this, "name");
-    private final StringProperty attributesProperty = new SimpleStringProperty(this, "attributes");
-
-    public LoginApp() {
-    }
+    private final ObjectProperty<User> userProperty = new SimpleObjectProperty<>(this, "user");
+    private final ObjectProperty<Throwable> errorProperty = new SimpleObjectProperty<>(this, "error");
 
     @Override
     public Route createRoute() {
+        Optional.ofNullable(LoginApp.class.getResource("/style.css"))
+                .map(URL::toExternalForm)
+                .ifPresent(css -> getScene().getStylesheets().add(css));
+
         // Google Auth provider
         final var googleAuth = AuthAPI.googleAuth()
                 .webAPI(getWebAPI())
@@ -72,25 +81,21 @@ public class LoginApp extends RouteApp {
 
         return Route.empty()
                 .and(getNode("/", (r) ->
-                        initView(googleAuth, googleCredentials,
+                        loginView(googleAuth, googleCredentials,
                                 microsoftAuth, microsoftCredentials)))
-                .and(getNode("/auth/google", (r) -> loginDataView()))
-                .and(getNode("/auth/microsoft", (r) -> loginDataView()))
+                .and(getNode("/auth/google", (r) -> authInfoView()))
+                .and(getNode("/auth/microsoft", (r) -> authInfoView()))
+                .and(getNode("/auth/error", (r) -> errorView()))
                 .filter(Filters.FullscreenFilter(true))
-                .filter(AuthFilters.create(googleAuth, googleCredentials, user -> {
-                    nameProperty.set("name: " + user.getName());
-                    attributesProperty.set("attributes: " + user.getAttributes());
-                }))
-                .filter(AuthFilters.create(microsoftAuth, microsoftCredentials, user -> {
-                    nameProperty.set("name: " + user.getName());
-                    attributesProperty.set("attributes: " + user.getAttributes());
-                }));
+                .filter(DevFilter.createDevFilter())
+                .filter(AuthFilters.oauth2(googleAuth, googleCredentials, userProperty::set, errorProperty::set))
+                .filter(AuthFilters.oauth2(microsoftAuth, microsoftCredentials, userProperty::set, errorProperty::set));
     }
 
-    public Node initView(GoogleAuthenticationProvider googleAuth,
-                         OAuth2Credentials googleCredentials,
-                         MicrosoftAuthenticationProvider microsoftAuth,
-                         OAuth2Credentials microsoftCredentials) {
+    public Node loginView(GoogleAuthenticationProvider googleAuth,
+                          OAuth2Credentials googleCredentials,
+                          MicrosoftAuthenticationProvider microsoftAuth,
+                          OAuth2Credentials microsoftCredentials) {
         GridPane gridPane = new GridPane();
         gridPane.setAlignment(Pos.CENTER);
         gridPane.setHgap(12.0);
@@ -148,18 +153,65 @@ public class LoginApp extends RouteApp {
         microsoftLoginButton.setOnAction(event ->
                 getWebAPI().openURL(microsoftAuth.authorizeUrl(microsoftCredentials)));
 
-        return new StackPane(gridPane);
+        final var stackPane = new StackPane(gridPane);
+        stackPane.getStyleClass().add("page");
+        return stackPane;
     }
 
-    public Node loginDataView() {
+    public Node authInfoView() {
+        final var headerLabel = new Label("User information:");
+        headerLabel.getStyleClass().add("header-label");
+
         Label nameLabel = new Label();
-        nameLabel.textProperty().bind(nameProperty);
+        nameLabel.textProperty().bind(Bindings.createStringBinding(() -> {
+            final var user = userProperty.get();
+            return user == null ? "" : user.getName();
+        }, userProperty));
         Label attributesLabel = new Label();
         attributesLabel.setWrapText(true);
-        attributesLabel.textProperty().bind(attributesProperty);
-        VBox pane = new VBox(nameLabel, attributesLabel);
-        pane.setSpacing(4.0);
-        pane.setStyle("-fx-background-color: limegreen");
-        return pane;
+        attributesLabel.textProperty().bind(Bindings.createStringBinding(() -> {
+            final var user = userProperty.get();
+            return user == null ? "" : user.getAttributes().toString();
+        }, userProperty));
+
+        final var pane = new VBox(headerLabel, nameLabel, attributesLabel);
+        pane.getStyleClass().add("auth-info-pane");
+
+        return new StackPane(pane);
+    }
+
+    public Node errorView() {
+        final var headerLabel = new Label("Something unexpected happen:");
+        headerLabel.getStyleClass().add("header-label");
+
+        final var errorLabel = new Label();
+        errorLabel.setWrapText(true);
+        errorLabel.getStyleClass().add("error-label");
+        errorLabel.textProperty().bind(Bindings.createStringBinding(() -> {
+            final Throwable throwable = errorProperty.get();
+            return throwable == null ? "" : throwable.getMessage();
+        }, errorProperty));
+
+        final var errorTextArea = new TextArea();
+        errorTextArea.getStyleClass().add("error-text-area");
+        VBox.setVgrow(errorTextArea, Priority.ALWAYS);
+        errorTextArea.textProperty().bind(Bindings.createStringBinding(() -> {
+            final Throwable throwable = errorProperty.get();
+            if (throwable == null) {
+                return "";
+            } else {
+                final StringWriter sw = new StringWriter();
+                final PrintWriter pw = new PrintWriter(sw);
+                throwable.printStackTrace(pw);
+                return sw.toString();
+            }
+        }, errorProperty));
+
+        final var pane = new VBox(headerLabel, errorLabel, errorTextArea);
+        pane.getStyleClass().add("error-pane");
+
+        final var stackPane = new StackPane(pane);
+        stackPane.getStyleClass().add("page");
+        return stackPane;
     }
 }
