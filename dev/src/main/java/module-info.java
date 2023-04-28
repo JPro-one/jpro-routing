bea689cfc3770d0420caa1062778bdb0fdc27f52
floriankirmaier <florian.kirmaier@gmail.com>
2023-04-28 19:39:39 +0200

    added missing requires

--- dev/src/main/java/module-info.java
+++ dev/src/main/java/module-info.java
@@ -4,10 +4,11 @@
  * @author Besmir Beqiri
  */
 open module one.jpro.routing.dev {
-    requires transitive simplefx.extended;
     requires transitive javafx.controls;
     requires one.jpro.routing.core;
     requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.evaicons;
+    requires org.kordamp.ikonli.ionicons4;
 
     exports one.jpro.routing.dev;
 }
