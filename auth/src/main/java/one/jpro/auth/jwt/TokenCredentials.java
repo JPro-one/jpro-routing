cc3ebe6ae5b94bc0a7c22e080ed4c2c7c6fcfcfd
Besmir Beqiri <besmir.beqiri@logicbind.io>
2023-05-03 12:14:17 +0200

    Move conversion static methods from the converter class to the parent class `TokenCredentials`

--- auth/src/main/java/one/jpro/auth/jwt/TokenCredentials.java
+++ auth/src/main/java/one/jpro/auth/jwt/TokenCredentials.java
@@ -25,6 +25,24 @@ import java.util.stream.Stream;
  */
 public class TokenCredentials implements Credentials {
 
+    /**
+     * Creates a token credentials object with the given JSON formatted string.
+     *
+     * @param json the string representation of the JSON object holding the token credential's information.
+     */
+    static TokenCredentials fromJSON(String json) {
+        return new TokenCredentials(new JSONObject(json));
+    }
+
+    /**
+     * Convert the token credential information to JSON format and provides it as a string.
+     *
+     * @return a string in JSON format.
+     */
+    static String toJSON(TokenCredentials tokenCredentials) {
+        return tokenCredentials.toJSON().toString();
+    }
+
     @NotNull
     private String token;
     @Nullable
